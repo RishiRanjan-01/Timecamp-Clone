b6fac7374eee0104069247417ef326e6346202dc
Rishi Ranjan <rishibhumi000@gmail.com>
2022-08-25 23:22:13 +0530

    Day_3 Code UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 
+import HomePage from './pages/HomePage';
 import Billing from './Sandeep/billing/Billing';
 import Tabbs from './Sandeep/tabs/Tabs';
 
 function App() {
   return (
     <div className="App">
-      <Tabbs/>
-     <Billing/>
+      {/* <Tabbs/>
+     <Billing/> */}
+     <HomePage/>
     </div>
   );
 }
